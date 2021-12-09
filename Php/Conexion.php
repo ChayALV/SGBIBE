92cd38b8641ff7bcbae182144fe0ef74709a461a
ChayALV <56765650+ChayALV@users.noreply.github.com>
2021-12-09 00:49:02 -0600

    Update Conexion.php

--- Php/Conexion.php
+++ Php/Conexion.php
@@ -3,14 +3,14 @@ $servername = "localhost";
 $username = "root";
 $password = "password";
 $bd = "sgdbibe";
+$mat = 'GABC7118';
+$passwordAlumno = md5($mat);
 // Conexion
 $conexion = new mysqli($servername, $username, $password, $bd);
 // Check connection
 if ($conexion->connect_error) {
   die("Connection failed: " . $conn->connect_error);
+  echo $passwordAlumno;
 }
 //echo "Connected successfully"
-$mat = 'GABC7118';
-$passwordAlumno = md5($mat);
-echo $$passwordAlumno;
 ?>
