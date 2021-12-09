2000e1147421a3733d28776c9194fb14cd442292
ChayALV <56765650+ChayALV@users.noreply.github.com>
2021-12-09 00:46:35 -0600

    Update Conexion.php

--- Php/Conexion.php
+++ Php/Conexion.php
@@ -10,4 +10,7 @@ if ($conexion->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 //echo "Connected successfully"
+$mat = 'GABC7118';
+$passwordAlumno = md5($mat);
+echo $$passwordAlumno;
 ?>
